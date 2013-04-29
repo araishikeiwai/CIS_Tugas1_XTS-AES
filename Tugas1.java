7dc90ead699f92d75830cea2bdd30b7cd93878d3
Ashar Fuadi <fushar@gmail.com>
2013-04-30 01:39:26 +0700

    Mengimplementasikan file chooser dan button

--- Tugas1.java
+++ Tugas1.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.awt.*;          
 import java.awt.event.*;    
 import javax.swing.*;       
@@ -6,8 +7,9 @@ import javax.swing.border.*;
 public class Tugas1 extends JFrame {
 	JPanel targetPanel, targetFilePanel, keyPanel, keyFilePanel, resultPanel, resultFilePanel, operationsPanel;
 	JLabel targetSelectLabel, targetNameLabel, keySelectLabel, keyNameLabel, resultLabel, resultNameLabel;
-	JFileChooser targetChooser, keyChooser, resultChooser;
 	JButton targetButton, keyButton, resultButton, encryptButton, decryptButton;
+
+	File targetFile, keyFile, resultFile;
 
 	public Tugas1() {
 		super("Assignment 1 CIS");
@@ -17,6 +19,14 @@ public class Tugas1 extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+		this.addTargetSection();
+		this.addKeySection();
+		this.addResultSection();
+		this.add(new JSeparator());
+		this.addOperationsSection();
+	}
+
+	private void addTargetSection() {
 		targetPanel = new JPanel();
 		targetPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		targetSelectLabel = new JLabel("Select target file:");
@@ -26,11 +36,22 @@ public class Tugas1 extends JFrame {
 		targetFilePanel = new JPanel();
 		targetFilePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		targetButton = new JButton("Browse");
+		targetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				JFileChooser fc = new JFileChooser();
+				if (fc.showOpenDialog(Tugas1.this) == JFileChooser.APPROVE_OPTION) {
+					targetFile = fc.getSelectedFile();
+					targetNameLabel.setText(targetFile.getName());
+				}
+			}
+		});
 		targetNameLabel = new JLabel();
 		targetFilePanel.add(targetButton);
 		targetFilePanel.add(targetNameLabel);
 		this.add(targetFilePanel);
+	}
 
+	private void addKeySection() {
 		keyPanel = new JPanel();
 		keyPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		keySelectLabel = new JLabel("Select key file:");
@@ -40,11 +61,22 @@ public class Tugas1 extends JFrame {
 		keyFilePanel = new JPanel();
 		keyFilePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		keyButton = new JButton("Browse");
+		keyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				JFileChooser fc = new JFileChooser();
+				if (fc.showOpenDialog(Tugas1.this) == JFileChooser.APPROVE_OPTION) {
+					keyFile = fc.getSelectedFile();
+					keyNameLabel.setText(keyFile.getName());
+				}
+			}
+		});
 		keyNameLabel = new JLabel();
 		keyFilePanel.add(keyButton);
 		keyFilePanel.add(keyNameLabel);
 		this.add(keyFilePanel);
+	}
 
+	private void addResultSection() {
 		resultPanel = new JPanel();
 		resultPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		resultLabel = new JLabel("Save output to:");
@@ -54,17 +86,37 @@ public class Tugas1 extends JFrame {
 		resultFilePanel = new JPanel();
 		resultFilePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		resultButton = new JButton("Browse");
+		resultButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				JFileChooser fc = new JFileChooser();
+				if (fc.showSaveDialog(Tugas1.this) == JFileChooser.APPROVE_OPTION) {
+					resultFile = fc.getSelectedFile();
+					resultNameLabel.setText(resultFile.getName());
+				}
+			}
+		});
 		resultNameLabel = new JLabel();
 		resultFilePanel.add(resultButton);
 		resultFilePanel.add(resultNameLabel);
 		this.add(resultFilePanel);
+	}
 
-		this.add(new JSeparator());
-
+	private void addOperationsSection() {
 		operationsPanel = new JPanel();
 		operationsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		encryptButton = new JButton("Encrypt");
 		decryptButton = new JButton("Decrypt");
+		ActionListener listener = new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				if (targetFile == null || keyFile == null || resultFile == null) {
+					JOptionPane.showMessageDialog(null, "Please specify target, key, and result files.");
+					return;
+				}
+				JOptionPane.showMessageDialog(null, "Ready!");
+			}
+		};
+		encryptButton.addActionListener(listener);
+		decryptButton.addActionListener(listener);
 		operationsPanel.add(encryptButton);
 		operationsPanel.add(decryptButton);
 		this.add(operationsPanel);
